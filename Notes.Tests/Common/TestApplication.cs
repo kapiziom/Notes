f65f0b0859d2817df5c604f34376093e209b9068
KTomczak <ktomczak1996@gmail.com>
2024-01-27 03:06:11 +0100

    fix TestApplication using

--- Notes.Tests/Common/TestApplication.cs
+++ Notes.Tests/Common/TestApplication.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Notes.Data;
 
 namespace Notes.Tests.Common;
